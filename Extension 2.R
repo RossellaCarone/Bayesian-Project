fb93d15a0a04fff30c96b91dcc06179be9d1f2db
Elisa Garlanda <elisa.garlanda@mail.polimi.it>
2024-02-14 15:41:21 +0100

    plots for extension 2

--- Extension 2.R
+++ Extension 2.R
@@ -8,8 +8,9 @@ library(emdbook)
 library(RcppArmadillo)
 library(splines2)
 library(mvnfast)
+library(coda)
 
-#### Rcpp functions ####
+#### Rcpp functions and auxiliary functions ####
 Rcpp::cppFunction("arma::mat armaInv(const arma::mat & x) { return arma::inv(x); }", depends="RcppArmadillo")
 Rcpp::cppFunction("arma::vec Arma_mvrnorm(const arma::vec& mean, const arma::mat& cov) {
   return arma::mvnrnd(mean, cov);}", depends="RcppArmadillo")
@@ -59,7 +60,7 @@ interp_spline <- function(x, y, nout = length(y)) {
 }
 
 
-
+#### Extension 2 ####
 extension2 <- function(y, niter = 1000, nburn = 1000, n_knots_h = 4, n_knots_m = 4,
                        a_eps = .5, b_eps = .2, a_phi = .1, b_phi = .1,v_psi=5,a1=1.5,a2=1.5,a_sigma=.5,b_sigma=0.25,
                        epsilon=1e-2,alpha_0=1.5,alpha_1=5*1e-4
@@ -466,9 +467,9 @@ extension2 <- function(y, niter = 1000, nburn = 1000, n_knots_h = 4, n_knots_m =
                 y_star=y_star))
 }
 
-library(fdasrvf)
-y<-growth_vel$f[,1:20]
-result<-extension2(y)
+# library(fdasrvf)
+# y<-growth_vel$f[,1:20]
+# result<-extension2(y)
 
 x11()
 par(mfrow=c(1,4))
@@ -477,3 +478,203 @@ matplot(result$post$mi_tilde, type='l', main='mi tilde')
 matplot(result$post$mi, type='l', main='mi')
 matplot(result$y_star, type='l', main='y*')
 
+
+#### Run the function ####
+a_phi=200
+b_phi=600
+a_eps=3000
+b_eps=5000
+n_knots_h=3
+n_knots_m=36
+
+#y= read.csv('Data_Undersampled_healthy.csv') #qua caricate il dataset con l'undersampling
+#y = read.csv('healthy_new_undersampling.csv')
+
+nburn = 50000
+niter = 15000
+start.time <- Sys.time()
+result = extension2(y=y, n_knots_h = n_knots_h, n_knots_m = n_knots_m, a_eps = a_eps, b_eps = b_eps,
+                    a_phi = a_phi, b_phi = b_phi, nburn = nburn, niter =niter)
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time,5)
+print(time.taken)
+
+
+
+#### Plot curves ####
+# mettere cex.lab = 1.2 e cex.axis=1.2 per ingrandire i valori sugli assi!!
+n_obs_max  = dim(y)[1] 
+n_patients = dim(y)[2] # y has patients in the columns
+y_list = lapply(seq_len(ncol(y)), function(i) na.omit(y[, i]))
+n_obs=sapply(y_list,length)
+
+col_pat = rainbow(n_patients)
+ylim_plot = c(0,90) #set ylim for the plot
+
+x11()
+plot(seq(0,1, len=n_obs[1]),y[1:n_obs[1],1], type='l', main='y', ylim=ylim_plot,
+     col=col_pat[1], xlab='time')
+for(i in 2:n_patients){
+  points(seq(0,1, len=n_obs[i]),y[1:n_obs[i],i], type='l', col=col_pat[i])
+}
+
+x11()
+plot(seq(0,1, len=n_obs[1]),result$y_star[1:n_obs[1],1], type='l', main='y*',
+     ylim=ylim_plot, col=col_pat[1], xlab='time')
+for(i in 2:n_patients){
+  points(seq(0,1, len=n_obs[i]),result$y_star[1:n_obs[i],i], type='l', col=col_pat[i])
+}
+
+
+x11()
+plot(seq(0,1, len=n_obs[1]),result$post$mi[1:n_obs[1],1], type='l', main='m_i', ylim=ylim_plot,
+     col=col_pat[1], xlab='time')
+for(i in 2:n_patients){
+  points(seq(0,1, len=n_obs[i]), result$post$mi[1:n_obs[i],i], type='l', col=col_pat[i])
+}
+
+x11()
+plot(seq(0,1, len=n_obs[1]),result$post$mi_tilde[1:n_obs[1],1], type='l', ylab='m_i[h_i(t)]', ylim=ylim_plot,
+     col=col_pat[1], xlab='t', cex.axis=1.2, cex.lab=1.2)
+for(i in 2:n_patients){
+  points(seq(0,1, len=n_obs[i]), result$post$mi_tilde[1:n_obs[i],i], type='l', col=col_pat[i])
+}
+
+
+
+#### Save structures ####
+acceptance_output = result$post$accepts
+k_output=result$full$k_s
+lambda_output=result$full$lambda_s
+psi_output = result$full$psi_s
+eta_output = result$full$eta_s
+theta_output = result$full$theta_s
+delta_output=result$full$delta_s
+tau_output = result$full$tau_s
+phi_output = result$full$phi_s
+sigma_phi_output = result$full$sigma_phi_s
+sigma_eps_output = result$full$sigma_eps_s
+sigmaj_output = result$full$sigma_j_s
+
+##### Traceplots + acceptance rate ####
+#Acceptance rate 
+x11()
+hist(acceptance_output, xlab = 'Rate', main='')
+
+x11()
+matplot(1:15000,k_ouput[],'l',main='k')
+
+x11()
+matplot(1:15000,k_output[(nburn+1):(niter+nburn)], 'l', ylab='', xlab='Iteration')
+
+
+#Phi
+x11()
+matplot(phi_output[(nburn+1):(niter+nburn),,1],type='l',xlab = 'Iteration', main='', ylab='')
+x11()
+matplot(phi_output[(nburn+1):(niter+nburn),,12],type='l',xlab = 'Iteration', main='', ylab='')
+
+
+#Sigma phi e sigma eps 
+x11()
+matplot(sigma_phi_output[(nburn+1):(niter+nburn)], type='l',xlab = 'Iteration', 
+        main='', ylab='')
+x11()
+matplot(sigma_eps_output[(nburn+1):(niter+nburn)], type='l',xlab = 'Iteration', 
+        main='', ylab='')
+
+
+#Delta 
+delta_out=sapply(delta_output, '[', 2)  #selezionare la componente desiderata 
+x11()
+matplot(delta_out[(nburn+1):(niter+nburn)], type='l',xlab = 'Iteration', 
+        main='', ylab='')
+
+#Tau
+tau_out=sapply(tau_output, '[', 2)  #selezionare la componente desiderata 
+x11()
+matplot(tau_out[(nburn+1):(niter+nburn)], type='l',xlab = 'Iteration', 
+        main='', ylab='')
+
+# sigma_j 
+x11()
+sigmaj_out=sapply(sigmaj_output, '[', 3)
+matplot(sigmaj_out[(nburn+1):(niter+nburn)], type='l',xlab = 'Iteration', 
+        main='', ylab='')
+
+
+#Theta (n patients X p)
+#theta_out=sapply(theta_output, [, 1)  #(questo seleziona l' elemento 1 da tutte le matrici) 
+#Per un paziente io devo selezionare tutta una riga--> devo avere p vettori 
+paz=7  #Scegliere il paziente 
+p=n_knots_m+4   #mettere il p usato 
+ind=(paz-1)*p+1#primo elemento paziente paz
+pos=8      #pos va da 0 a p-1 (0--> prima componente, p-1-->ultima componente) 
+ind=ind+pos
+theta_out=sapply(theta_output, '[',66) #theta del paziente 2, terzo p
+x11()
+matplot(theta_out[(nburn+1):(niter+nburn)],type='l',main='', ylab='', xlab='Iteration')
+
+# p = 3
+# n_pat = 2
+# k = 1
+
+#Lambda (p X k)
+#65000, ognuna 40x17
+#lambda_output[[2]]
+#head(l_out)
+#,3 significa terza riga, prima colonna della matrice
+l_out=sapply(lambda_output, '[',3)   #selezionare la componente (il massimo è max(k)*p)
+x11()
+matplot(l_out[(nburn+1):(niter+nburn)],type='l', ylab='', main='', xlab='Iteration')
+
+#Psi (p X k)
+psi_out=sapply(psi_output, '[',3)   #selezionare la componente (il massimo è max(k)*p)
+x11()
+matplot(psi_out[(nburn+1):(niter+nburn)],type='l',main='', ylab='')
+
+#Eta (n_patients X k)
+#Per un paziente  devo selezionare tutta una riga--> devo avere k vettori 
+paz=7  #Scegliere il paziente 
+k_act=14  #mettere il k usato (cambia!!!) 
+ind=(paz-1)*k_act+1#primo elemento paziente paz
+pos=2        #pos va da 0 a k-1 (0--> prima componente, k-1-->ultima componente) 
+ind=ind+pos
+eta_out=sapply(eta_output, '[',2) #paziente 2, prima componente di k
+x11()
+matplot(eta_out[(nburn+1):(niter+nburn)],type='l',main='', ylab='')
+
+
+#graphics.off()
+
+
+#### Diagnostics  - create MCMC objects + ACF ####
+sigma_phi_mcmc = mcmc(sigma_phi_output[-c(1:nburn)])
+sigma_eps_mcmc = mcmc(sigma_eps_output[-c(1:nburn)])
+lambda_mcmc = mcmc(l_out[-c(1:nburn)])
+psi_mcmc = mcmc(psi_out[-c(1:nburn)])
+tau_mcmc = mcmc(tau_out[-c(1:nburn)])
+eta_mcmc = mcmc(eta_out[-c(1:nburn)])
+delta_mcmc = mcmc(delta_out[-c(1:nburn)])
+theta_mcmc = mcmc(theta_out[-c(1:nburn)])
+sigmaj_mcmc = mcmc(sigmaj_out[-c(1:nburn)])
+
+# acf
+x11()
+acf(sigma_phi_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(sigma_eps_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(lambda_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(psi_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(tau_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(eta_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(delta_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(theta_mcmc, lag.max=500, main = '', ylab = '')
+x11()
+acf(sigmaj_mcmc, lag.max=500, main = '', ylab = '')
